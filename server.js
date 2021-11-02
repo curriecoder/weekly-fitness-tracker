98c7f2a063de6c39b06d262a92dfbe47755be427
curriecoder <andrew.j.currie0@gmail.com>
2021-11-02 12:15:01 -0400

    preparing to deploy to heroku

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(express.json());
 // Static for index.html
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
 
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
